a78955569f3150bf739ca54e5398f5de3acad5e5
Dhyeonle9 <dhyeon.le9@gmail.com>
2023-11-09 14:02:24 +0900

    accounts/signup,login,logout 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts_form.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+
+            return redirect('posts:index')
+
+    else:
+        form = CustomAuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts_form.html', context)
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')

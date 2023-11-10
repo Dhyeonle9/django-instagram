343762060637497bb61fdafa626c64ca9d6c9890
Dhyeonle9 <dhyeon.le9@gmail.com>
2023-11-10 10:46:35 +0900

    좋아요기능 구현

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     posts = Post.objects.all().order_by('-id')
@@ -9,6 +10,7 @@ def index(request):
     }
     return render(request,'index.html', context)
 
+@login_required
 def create(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
@@ -30,3 +32,17 @@ def detail(request, id):
         'post': post
     }
     return render(request, 'detail.html', context)
+
+@login_required
+def likes(request, id):
+    user = request.user
+    post = Post.objects.get(id=id)
+    # 이미 좋아요 버튼을 누른경우
+    if post in user.like_posts.all():
+        post.like_users.remove(user)
+    # if user in post.like_users.all()
+    # 좋아요 버튼을 아직 누르지 않은경우
+    else:
+        post.like_users.add(user)
+        # == user.like_posts.add(post)
+    return redirect('posts:index')

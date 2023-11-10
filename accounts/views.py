e6d5f52cf0c4f8cd6ddccbc7bc035e23634792e4
Dhyeonle9 <dhyeon.le9@gmail.com>
2023-11-10 14:06:32 +0900

    수업실습

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from .models import User
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
 
 def signup(request):
     if request.method == 'POST':
